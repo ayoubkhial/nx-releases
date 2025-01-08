c1a0830f7aa36feb35b6c12f5dfd2c9919f4ade0
Ayoub KHIAL <ayoubcodez@gmail.com>
2025-01-08 23:18:58 +0100

    feat: call libraries in docs app

--- apps/docs/src/app/app.component.ts
+++ apps/docs/src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ButtonComponent } from '@org-ayb/button';
+import { TooltipComponent } from '@org-ayb/tooltip';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    ButtonComponent,
+    TooltipComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
