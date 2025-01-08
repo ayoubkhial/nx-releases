cb9771e7bb42054aeeda3370024e827f4adad72c
Ayoub KHIAL <ayoubcodez@gmail.com>
2025-01-08 23:07:12 +0100

    chore: create libraries

---
+++ packages/tooltip/src/lib/tooltip/tooltip.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-tooltip',
+  imports: [CommonModule],
+  templateUrl: './tooltip.component.html',
+  styleUrl: './tooltip.component.css',
+})
+export class TooltipComponent {}
